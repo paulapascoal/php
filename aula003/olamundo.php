9984d7f74153dd4c7ab65ed13757614da352946c
paulapascoal <paulaiasmimpascoal2012@gmail.com>
2025-05-27 11:10:01 -0300

    aula003

---
+++ aula003/olamundo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo de PHP</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        h2{
+            color: blue;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+
+    <?php
+
+    echo "<h2> Olá, <br> Mundo!</h2>";
+    echo "<strong> Professor Gustavo Gunabara é o melhor professor da Internet!!! </strong>"
+
+
+    ?>
+
+    <footer>
+        <p>&copy; Curso em Vìdeo</p>
+    </footer>
+    
+</body>
+</html>
--- aula003/olamundo/ola.php
+++ aula003/olamundo/ola.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Primeiro exemplo PHP</title>
-    <style>
-        h2{
-            color: blue;
-            text-shadow: 1px 1px 1px black;
-        }
-    </style>
-</head>
-<body>
-    <h1>Testando PHP</h1>
-    <?php
-        echo "<h2>Olá,<br> Mundo!</h2>";
-        echo "<strong>Professor Gustavo Guanabara é o melhor da internet!!!</strong>"
-
-
-    ?>
          <footer>
         <p> &copy; CursoemVídeo </p>
     </footer>
-    
-    
-</body>
-</html>
